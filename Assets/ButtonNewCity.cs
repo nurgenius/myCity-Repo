0c33eb38f5cf735ea81f84565a922a729021bc15
nurgenius <4nurkids@gmail.com>
2015-03-28 15:02:51 +0300

    dsd
    sdsd

---
+++ Assets/ButtonNewCity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonNewCity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+	
+}
